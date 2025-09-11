a0d0eb8f93d023c80fab7f0d6b3d41334626cb3f
bodnar-fasolka <bodnar.solomiya.v@gmail.com>
2025-09-11 21:30:16 +0900

    Remove cards: Маркетингові дослідження Стратегічне планування Digital маркетинг Брендинг Консалтинг

--- src/components/ServicesSection.tsx
+++ src/components/ServicesSection.tsx
@@ -35,31 +35,6 @@ const ServicesSection = () => {
         { name: t('ppcService'), slug: "ppc" },
         { name: t('metaAdsService'), slug: "meta-ads" }
       ]
-    },
-    {
-      title: "Маркетингові дослідження",
-      description: "Глибокий аналіз ринку та цільової аудиторії",
-      icon: <BarChart3 className="w-8 h-8" />
-    },
-    {
-      title: "Стратегічне планування",
-      description: "Розробка довгострокової маркетингової стратегії",
-      icon: <Target className="w-8 h-8" />
-    },
-    {
-      title: "Digital маркетинг",
-      description: "Просування в онлайн каналах та соціальних мережах",
-      icon: <Megaphone className="w-8 h-8" />
-    },
-    {
-      title: "Брендинг",
-      description: "Створення та розвиток бренду компанії",
-      icon: <TrendingUp className="w-8 h-8" />
-    },
-    {
-      title: "Консалтинг",
-      description: "Експертні поради з маркетингових питань",
-      icon: <Users className="w-8 h-8" />
     }
   ];
 
